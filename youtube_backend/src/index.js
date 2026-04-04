d0ce0a3a356806adf7b661cb81106728dd9135c2
Aditya Kumawat <adityakumawat2003@gmail.com>
2026-04-04 11:54:11 +0530

    DB connections

--- youtube_backend/src/index.js
+++ youtube_backend/src/index.js
@@ -1,6 +1,12 @@
-import mongoose from "mongoose";
+import  dotenv from "dotenv"
+import mongoose, { connect } from "mongoose";
 import {DB_NAME} from "./constants.js"
+import connectDB from "./db/index.js";
 
+dotenv.config({
+    path:"./.env"
+})
+connectDB();
 
 
 
